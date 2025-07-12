de927beed77bbea6b60c589fa23dea85fcf9125f
AGUNAN <agunan11@gmail.com>
2025-07-12 13:41:47 +0530

    folder creation

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1 className='bg-purple-500'>Image Generator</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
